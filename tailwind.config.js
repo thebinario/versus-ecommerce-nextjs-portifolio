13c1499e40e01bcdda9b55eda99c8fc3c89628b2
Abimael Ferreira <abimaelfrrr@gmail.com>
2024-08-03 00:10:04 -0400

    Criando paletas de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,28 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#6c63ff',  // roxo claro
+          DEFAULT: '#3f3d56',  // roxo padrão
+          dark: '#2e2c3a'  // roxo escuro
+        },
+        secondary: {
+          light: '#ff6584',
+          DEFAULT: '#ff4561',
+          dark: '#cc374d'
+        },
+        background: {
+          light: '#ffffff',
+          dark: '#1a202c'
+        },
+        text: {
+          light: '#2d3748',
+          dark: '#edf2f7'
+        }
+      }
+    },
   },
   plugins: [],
 };
