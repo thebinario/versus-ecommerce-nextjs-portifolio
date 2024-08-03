0ba4c1befb06275f4b5cce140e740cb1966d07e5
Abimael Ferreira <abimaelfrrr@gmail.com>
2024-08-03 01:01:55 -0400

    Ajustando layout para o cart funcionar corretamente entre todos oc omponente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
+// src/app/layout.tsx ou src/app/(root)/layout.tsx dependendo da estrutura do seu projeto
 import 'tailwindcss/tailwind.css';
 import './globals.css';
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header';
+import { CartProvider } from '@/context/CartContext';
 
-
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="pt">
         <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>Versus</title>
-            <meta name="description" content='Versus'/>
+            <meta name="description" content="Versus" />
         </head>
         <body>
-        <Header/>
-        <main>{children}</main>
-        <Footer/>
+        <CartProvider>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </CartProvider>
         </body>
         </html>
     );
